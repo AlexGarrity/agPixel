29eba91b981116c4b6902f4d0f7387076a4619c4
Alex Garrity <garrity2799@gmail.com>
2020-01-18 11:02:23 +0000

    Fixed a small error in Pixel.hpp and Array.hpp

--- include/Array.hpp
+++ include/Array.hpp
@@ -3,3 +3,6 @@
 // Header to include all forms of pixel array
 // Most likely unnecessary as you shouldn't mix representations
 
+#include "Array/Double.hpp"
+#include "Array/Float.hpp"
+#include "Array/Int.hpp"
--- include/Pixel.hpp
+++ include/Pixel.hpp
@@ -3,6 +3,6 @@
 // Header to include all forms of pixel
 // Most likely not required, as you shouldn't really mix representations
 
-#include "Pixel/PixelDouble.hpp"
-#include "Pixel/PixelFloat.hpp"
-#include "Pixel/PixelInt.hpp"
+#include "Pixel/Double.hpp"
+#include "Pixel/Float.hpp"
+#include "Pixel/Int.hpp"
